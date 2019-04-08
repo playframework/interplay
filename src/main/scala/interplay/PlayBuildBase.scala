029d6293cb9bb86a172dc2c7408f75323524aa84
Marcos Pereira <marcos.silva@gmail.com>
2019-04-08 11:38:25 +0000

    Scala 2.13.0-RC1 support

--- src/main/scala/interplay/PlayBuildBase.scala
+++ src/main/scala/interplay/PlayBuildBase.scala
@@ -18,7 +18,7 @@ import sbtwhitesource.WhiteSourcePlugin.autoImport._
 object ScalaVersions {
   val scala210 = "2.10.7"
   val scala212 = "2.12.8"
-  val scala213 = "2.13.0-M5"
+  val scala213 = "2.13.0-RC1"
 }
 
 object SbtVersions {
