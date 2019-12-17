d138d2803b5686ac5ce7dd84334139dd3696977a
Marcos Pereira <marcos.silva@gmail.com>
2019-12-17 17:11:59 -0500

    crossScalaVersion := Nil for root projects

--- src/main/scala/interplay/PlayRootProjectBase.scala
+++ src/main/scala/interplay/PlayRootProjectBase.scala
@@ -1,6 +1,7 @@
 package interplay
 
 import sbt._
+import sbt.Keys._
 
 /**
  * Base Plugin for a root project that doesn't get published.
@@ -10,5 +11,7 @@ import sbt._
 object PlayRootProjectBase extends AutoPlugin {
   override def trigger = noTrigger
   override def requires = PlayBuildBase && PlayBintrayBase && PlaySonatypeBase && PlayReleaseBase
-  override def projectSettings = PlayNoPublishBase.projectSettings
+  override def projectSettings = PlayNoPublishBase.projectSettings ++ Seq(
+    crossScalaVersions := Nil
+  )
 }
