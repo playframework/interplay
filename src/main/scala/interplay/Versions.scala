6afd64e8f1ca5cca95833e380eabef3f4b131e4f
Matthias Kurz <m.kurz@irregular.at>
2020-04-23 13:06:21 +0200

    Scala 2.13.2 / 2.12.11 & sbt 1.3.10

--- src/main/scala/interplay/Versions.scala
+++ src/main/scala/interplay/Versions.scala
@@ -1,11 +1,11 @@
 package interplay
 
 object ScalaVersions {
-  val scala212 = "2.12.10"
-  val scala213 = "2.13.1"
+  val scala212 = "2.12.11"
+  val scala213 = "2.13.2"
 }
 
 object SbtVersions {
   val sbt10 = "1.2.8"
-  val sbt13 = "1.3.6"
+  val sbt13 = "1.3.10"
 }
