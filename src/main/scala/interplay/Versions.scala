40d8f98e33ad3fdf7d3684d76a5d9582754c9b08
Matthias Kurz <m.kurz@irregular.at>
2021-02-19 11:27:49 +0100

    Upgrade versions

--- src/main/scala/interplay/Versions.scala
+++ src/main/scala/interplay/Versions.scala
@@ -1,11 +1,11 @@
 package interplay
 
 object ScalaVersions {
-  val scala212 = "2.12.11"
-  val scala213 = "2.13.2"
+  val scala212 = "2.12.13"
+  val scala213 = "2.13.5"
 }
 
 object SbtVersions {
   val sbt10 = "1.2.8"
-  val sbt13 = "1.3.10"
+  val sbt13 = "1.3.13"
 }
