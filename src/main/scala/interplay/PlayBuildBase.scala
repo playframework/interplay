a64cb689f2632464bfb67a67b4d771a81fc1b51f
Scala Steward <me@scala-steward.org>
2021-05-17 18:55:15 +0200

    Update scala-library to 2.13.6
    (cherry picked from commit 0f712a4b32c10634bc31bb80ae014854fefade72)

--- src/main/scala/interplay/PlayBuildBase.scala
+++ src/main/scala/interplay/PlayBuildBase.scala
@@ -17,7 +17,7 @@ import sbtwhitesource.WhiteSourcePlugin.autoImport._
 object ScalaVersions {
   val scala210 = "2.10.7"
   val scala212 = "2.12.13"
-  val scala213 = "2.13.5"
+  val scala213 = "2.13.6"
 }
 
 object SbtVersions {
