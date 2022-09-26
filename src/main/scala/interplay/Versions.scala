074983ba4aaaf7614283cc1f2f6879c9957646b7
Matthias Kurz <m.kurz@irregular.at>
2022-09-26 10:46:58 +0200

    new versions

--- src/main/scala/interplay/Versions.scala
+++ src/main/scala/interplay/Versions.scala
@@ -1,11 +1,11 @@
 package interplay
 
 object ScalaVersions {
-  val scala212 = "2.12.15"
-  val scala213 = "2.13.8"
-  val scala3   = "3.1.0"
+  val scala212 = "2.12.17"
+  val scala213 = "2.13.9"
+  val scala3   = "3.2.0"
 }
 
 object SbtVersions {
-  val sbt16 = "1.6.1"
+  val sbt17 = "1.7.1"
 }
