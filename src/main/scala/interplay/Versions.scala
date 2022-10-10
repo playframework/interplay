8727ba784422d6de5897a34f0604e9a72a52ac3e
Matthias Kurz <m.kurz@irregular.at>
2022-10-10 10:36:15 +0200

    Latest Scala + sbt versions

--- src/main/scala/interplay/Versions.scala
+++ src/main/scala/interplay/Versions.scala
@@ -2,10 +2,10 @@ package interplay
 
 object ScalaVersions {
   val scala212 = "2.12.17"
-  val scala213 = "2.13.9"
+  val scala213 = "2.13.10"
   val scala3   = "3.2.0"
 }
 
 object SbtVersions {
-  val sbt17 = "1.7.1"
+  val sbt17 = "1.7.2"
 }
