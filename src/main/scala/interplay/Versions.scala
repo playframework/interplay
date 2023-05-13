44dbfaa8b073c2881d5bf7e9b67ff208f40151cc
Matthias Kurz <m.kurz@irregular.at>
2023-05-13 16:45:50 +0200

    Scala 3.3.0-RC6 + sbt 1.9.0-RC2

--- src/main/scala/interplay/Versions.scala
+++ src/main/scala/interplay/Versions.scala
@@ -3,9 +3,9 @@ package interplay
 object ScalaVersions {
   val scala212 = "2.12.17"
   val scala213 = "2.13.10"
-  val scala3   = "3.3.0-RC5"
+  val scala3   = "3.3.0-RC6"
 }
 
 object SbtVersions {
-  val sbt19 = "1.9.0-RC1"
+  val sbt19 = "1.9.0-RC2"
 }
