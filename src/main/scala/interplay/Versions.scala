3467a58f9092a2a0a29452e704a1c7a3656d723f
Matthias Kurz <m.kurz@irregular.at>
2023-06-03 22:32:28 +0200

    sbt 1.9.0 / Scala 2.13.11 + 2.12.18

--- src/main/scala/interplay/Versions.scala
+++ src/main/scala/interplay/Versions.scala
@@ -1,11 +1,11 @@
 package interplay
 
 object ScalaVersions {
-  val scala212 = "2.12.18-M2"
-  val scala213 = "2.13.11-M2"
+  val scala212 = "2.12.18"
+  val scala213 = "2.13.11"
   val scala3   = "3.3.0"
 }
 
 object SbtVersions {
-  val sbt19 = "1.9.0-RC3"
+  val sbt19 = "1.9.0"
 }
